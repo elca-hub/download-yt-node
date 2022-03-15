c226b6070599523750599035823e4b6585a59b68
poyuaki <101086816+m0k1moki@users.noreply.github.com>
2022-03-15 16:51:03 +0900

    現在再生中の曲にボーダーを付与

--- test/youtube-speaker/src/components/molecule/SongListItemMolecule.vue
+++ test/youtube-speaker/src/components/molecule/SongListItemMolecule.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="song-list-item-molecule" @click="changePlaySong(item.id)">
+  <div class="song-list-item-molecule" @click="changePlaySong(item.id)" :class="{'active': isPlaying}">
     <h2>{{ item.title }}</h2>
     <p>{{ item.author }}</p>
   </div>
@@ -16,6 +16,15 @@ export default class SongListItemMolecule extends Vue {
   
   public changePlaySong (id: string) {
     this.$store.dispatch('setAudioObj', id);
+  }
+
+  get isPlaying () {
+    const nowPlayingSongItem = this.$store.getters.getNowPlayingVideoItem;
+    if (nowPlayingSongItem !== null) {
+      return nowPlayingSongItem.id === this.item.id;
+    } else {
+      return false;
+    }
   }
 }
 </script>
@@ -38,5 +47,9 @@ export default class SongListItemMolecule extends Vue {
     margin: 0;
     text-align: right;
   }
+}
+
+.active {
+  border-bottom: solid 1px #777;
 }
 </style>
