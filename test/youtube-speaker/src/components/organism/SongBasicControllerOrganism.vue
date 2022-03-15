608f79468db6680698ebf1f72310eae515d671fc
poyuaki <101086816+m0k1moki@users.noreply.github.com>
2022-03-15 14:27:23 +0900

    基本操作GUIのコンポーネント化

---
+++ test/youtube-speaker/src/components/organism/SongBasicControllerOrganism.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="song-basic-controller-organism">
+    <div class="skip-before" @click="$store.dispatch('skipSong', false)">
+      <i class="bi bi-skip-backward" />
+    </div>
+    <div class="song-play" @click="$store.dispatch('songControll', !$store.state.isPlaying)">
+      <i class="bi bi-play" /><i class="bi bi-slash-lg" /><i class="bi bi-pause" />
+    </div>
+    <div class="skip-after" @click="$store.dispatch('skipSong', true)">
+      <i class="bi bi-skip-forward" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SongBasicControllerOrganism extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+.song-basic-controller-organism {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+</style>
