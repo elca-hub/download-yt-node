b768cb75ba972ceb27680b4c99805c6e6acebd56
poyuaki <101086816+m0k1moki@users.noreply.github.com>
2022-03-19 17:20:05 +0900

    dbの構成を変更

--- db/sql/001-create-tables.sql
+++ db/sql/001-create-tables.sql
@@ -1,11 +1,11 @@
 create database if not exists yt_song;
 use yt_song;
-DROP TABLE IF EXISTS `yt_song_data`;
-create table IF not exists `yt_song_data`
+DROP TABLE IF EXISTS `songs`;
+create table IF not exists `songs`
 (
- `id`               INT(20) AUTO_INCREMENT,
- `name`             VARCHAR(20) NOT NULL,
- `created_at`       Datetime DEFAULT NULL,
- `updated_at`       Datetime DEFAULT NULL,
+  `id` INT(20) AUTO_INCREMENT,
+  `title` VARCHAR(100) NOT NULL,
+  `author` VARCHAR(100) NOT NULL,
+  `sort_index` INT(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
